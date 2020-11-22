d2263e09df36973408efcd6e2ebd6953a944493e
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-22 18:23:58 +0700

    :atom: New Web

--- script1.js
+++ script1.js
@@ -1,12 +1,12 @@
-function logger(){
-    console.log(test1(1,4))
-    console.log(test2(5,4))
+function logger() {
+    console.log(test1(1, 4))
+    console.log(test2(5, 4))
 }
 
-function test1(a,b){
-    return a+b;
+function test1(a, b) {
+    return a + b;
 }
 
-function test2(a,b){
-    return a-b;
+function test2(a, b) {
+    return a - b;
 }
