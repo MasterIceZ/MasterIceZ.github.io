902a923c6be584d7648038a5584b91b0b7d4d5e7
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-08-31 23:36:08 +0700

    feat: y3 as of 31 aug

--- src/data/courseData.ts
+++ src/data/courseData.ts
@@ -17,7 +17,7 @@ export const courseData = [
   },
   {
     code: 2110211,
-    subject: "Data Structure",
+    subject: "Intro to Data Structure",
     repository: ["2110211"],
   },
   {
@@ -43,6 +43,11 @@ export const courseData = [
   {
     code: 2110514,
     subject: "Realtime Computer Graphics and Physics Simulation",
-    repository: ["2110514", "SPH-FluidSimulation.git"],
+    repository: ["2110514", "SPH-FluidSimulation"],
+  },
+  {
+    code: 2110366,
+    subject: "Embedded System Laboratory I",
+    repository: ["2110366"],
   },
 ] as ICourse[];
